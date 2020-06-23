1c624b818e5d513cddc0ddf5e8269b8367723435
PJ <pacojuanquiros@gmail.com>
2020-06-23 10:03:30 +0200

    Orthographic camera zNear and zFar

--- Xaloc/src/Xaloc/Renderer/OrthographicCamera.cpp
+++ Xaloc/src/Xaloc/Renderer/OrthographicCamera.cpp
@@ -5,15 +5,15 @@
 
 namespace Xaloc {
 
-	OrthographicCamera::OrthographicCamera(float left, float right, float bottom, float top)
-		: m_ProjectionMatrix(glm::ortho(left, right, bottom, top, -1.0f, 1.0f)), m_ViewMatrix(1.0f)
+	OrthographicCamera::OrthographicCamera(float left, float right, float bottom, float top, float zNear, float zFar)
+		: m_ProjectionMatrix(glm::ortho(left, right, bottom, top, zNear, zFar)), m_ViewMatrix(1.0f)
 	{
 		m_ViewProjectionMatrix = m_ProjectionMatrix * m_ViewMatrix;
 	}
 
-	void OrthographicCamera::SetProjection(float left, float right, float bottom, float top)
+	void OrthographicCamera::SetProjection(float left, float right, float bottom, float top, float zNear, float zFar)
 	{
-		m_ProjectionMatrix = glm::ortho(left, right, bottom, top, -1.0f, 1.0f);
+		m_ProjectionMatrix = glm::ortho(left, right, bottom, top, zNear, zFar);
 		m_ViewProjectionMatrix = m_ProjectionMatrix * m_ViewMatrix;
 	}
 
